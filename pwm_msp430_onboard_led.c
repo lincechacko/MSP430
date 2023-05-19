6dc78aa0d2e9b9d5255a7c24437f57a476bc29a4
Lince Kuruvila Chacko <63118804+lincechacko@users.noreply.github.com>
2023-05-19 17:54:51 +0530

    Update pwm_msp430_onboard_led.c

--- pwm_msp430_onboard_led.c
+++ pwm_msp430_onboard_led.c
@@ -11,7 +11,7 @@ int main(void)
     P1SEL1 |= BIT1;
 
 
-    TA0CCR0 = 1000; //Set the period in the Timer A0 Capture/Compare 0 register to 1000 us.
+    TA0CCR0 = 1000-1; //Set the period in the Timer A0 Capture/Compare 0 register to 1000 us.
     TA0CCTL1 = OUTMOD_7;
     TA0CCR1 = 1000; //The period in microseconds that the power is ON. It's half the time, which translates to a 50% duty cycle.
     TA0CTL = TASSEL_2 + MC_1; //TASSEL_2 selects SMCLK as the clock source, and MC_1 tells it to count up to the value in
